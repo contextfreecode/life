719b51384a50532780733128548c2675eddba770
Tom <tom@contextfree.info>
2026-06-03 05:44:36 -0700

    Start

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/contextfreecode/life
+
+go 1.25.0
---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+
+
+def main():
+    print("Hello from life!")
+    pygame.init()
+    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
