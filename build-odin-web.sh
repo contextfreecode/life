0784df5f8c9afa7ec2ada5d3e6a047e8f101b77a
Tom <tom@contextfree.info>
2026-06-12 20:33:55 -0700

    Include built web demo

--- build-odin-web.sh
+++ build-odin-web.sh
@@ -1,16 +1,18 @@
 #!/bin/bash -eu
 EMSCRIPTEN_SDK_DIR="$HOME/apps/emsdk"
+OUT_DIR=build/web
+mkdir -p $OUT_DIR
 export EMSDK_QUIET=1
 [[ -f "$EMSCRIPTEN_SDK_DIR/emsdk_env.sh" ]] && . "$EMSCRIPTEN_SDK_DIR/emsdk_env.sh"
 # Note RAYLIB_WASM_LIB=env.o -- env.o is an internal WASM object file. You can
 # see how RAYLIB_WASM_LIB is used inside <odin>/vendor/raylib/raylib.odin.
 # The emcc call will be fed the actual raylib library file. That stuff will end
 # up in env.o
-odin build web -o:speed -target:js_wasm32 -build-mode:obj -define:RAYLIB_WASM_LIB=env.o -define:RAYGUI_WASM_LIB=env.o -out:life.wasm.o
+odin build web -o:speed -target:js_wasm32 -build-mode:obj -define:RAYLIB_WASM_LIB=env.o -define:RAYGUI_WASM_LIB=env.o -out:$OUT_DIR/life.wasm.o
 ODIN_PATH=$(odin root)
-cp $ODIN_PATH/core/sys/wasm/js/odin.js .
-files="life.wasm.o ${ODIN_PATH}/vendor/raylib/wasm/libraylib.a ${ODIN_PATH}/vendor/raylib/wasm/libraygui.a"
+cp $ODIN_PATH/core/sys/wasm/js/odin.js $OUT_DIR
+files="$OUT_DIR/life.wasm.o ${ODIN_PATH}/vendor/raylib/wasm/libraylib.a ${ODIN_PATH}/vendor/raylib/wasm/libraygui.a"
 flags="-sEXPORTED_RUNTIME_METHODS=['HEAPF32'] -sUSE_GLFW=3 -sWASM_BIGINT -sWARN_ON_UNDEFINED_SYMBOLS=0 -sASSERTIONS --shell-file web/emscripten_template.html"
 # For debugging: Add `-g` to `emcc` (gives better error callstack in chrome)
-emcc -o emscripten.html $files $flags
-rm life.wasm.o
+emcc -o $OUT_DIR/emscripten.html $files $flags
+rm $OUT_DIR/life.wasm.o
--- run-odin.sh
+++ run-odin.sh
@@ -2,5 +2,8 @@ set -e
 # For RPi, build raylib with this:
 # time make PLATFORM=PLATFORM_DESKTOP GRAPHICS=GRAPHICS_API_OPENGL_21
 # flags=-o:speed
-time odin build native $flags -extra-linker-flags="-lGL -lEGL -lX11" -out:life
-./life
+OUT_DIR=build/native
+mkdir -p $OUT_DIR
+time odin build native $flags -extra-linker-flags="-lGL -lEGL -lX11" \
+    -out:$OUT_DIR/life
+$OUT_DIR/life
